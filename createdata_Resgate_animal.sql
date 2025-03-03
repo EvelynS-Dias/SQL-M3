1ac103061c26ae892ca36942f3a89591885a16bd
jhope122 <giselegomes0100@gmail.com>
2025-03-03 18:12:31 -0300

    :feat: Criacao da tabela apadrinhamento e doador

--- createdata_Resgate_animal.sql
+++ createdata_Resgate_animal.sql
@@ -90,3 +90,46 @@ CREATE TABLE Abrigo (
     qntd_animais INT NOT NULL,
     limite_animais INT NOT NULL
 );
+
+
+
+CREATE TABLE Adotante (
+    id_adotante INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL,  
+    rg VARCHAR(12) NOT NULL,       
+    endereco_completo VARCHAR(100) NOT NULL,  
+    email VARCHAR(100) UNIQUE NOT NULL,  
+    telefone VARCHAR(15) NOT NULL  
+);
+
+
+CREATE TABLE Doacao (
+    id_doacao INT AUTO_INCREMENT PRIMARY KEY,
+    id_doador INT NOT NULL,
+    id_clinica INT NOT NULL,
+    valor DECIMAL(10, 2) CHECK (valor >= 0),
+    tipo_doacao ENUM('dinheiro', 'alimentos', 'cobertores', 'medicamentos', 'outros') NOT NULL,
+    extrato_bancario TEXT,
+    FOREIGN KEY (id_doador) REFERENCES Adotante(id_adotante),
+    FOREIGN KEY (id_clinica) REFERENCES Clinica(id_clinica)
+);
+
+create table apadrinhamento(
+id_apadrinhamento INT AUTO_INCREMENT PRIMARY KEY,
+valor decimal(10,2) CHECK (valor >=0),
+data_ DATE,
+id_animal INT,
+id_padrinho INT,
+FOREIGN KEY (id_animal) REFERENCES animal(id_animal),
+FOREIGN KEY (id_padrinho) REFERENCES adotante(id_adotante)
+);
+
+
+
+create table doador( 
+id_adotante INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR (150),
+cpf CHAR (11) UNIQUE NOT NULL,
+rg CHAR (10) UNIQUE NOT NULL
+);
